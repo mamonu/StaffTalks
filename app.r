e54c61efe57c6728bd01cc3948bdceafbfe08073
mamonu <thorosm@msn.com>
2016-11-11 13:35:39 +0000

    added LDA

--- app.r
+++ app.r
@@ -156,6 +156,158 @@ server <- function(input, output) {
   })
   
   
+  
+  
+  output$LDA <-  renderFormattable      ({
+    
+    
+    
+    inFile <- input$file1
+    
+    #if (is.null(inFile))
+    # return (NULL)
+    
+    #read.csv(inFile$datapath, header = input$header,
+    #         sep = input$sep, quote = input$quote)
+    
+    data <-read.csv2(inFile$datapath, sep="\n", stringsAsFactors=FALSE,header = FALSE)
+    
+    
+    
+    
+    
+    #######
+    
+    desc <- gsub("'", "", data)  # remove apostrophes
+    desc <- gsub("[[:punct:]]", "", desc)  # replace punctuation with space
+    desc <- gsub("[[:cntrl:]]", "", desc)  # replace control characters with space
+    desc <- gsub("[[:digit:]]+", "", desc) # remove numbers
+    
+    desc <- gsub("^[[:space:]]+", "", desc) # remove whitespace at beginning of documents
+    desc <- gsub("[[:space:]]+$", "", desc) # remove whitespace at end of documents
+    desc <- tolower(desc)  #
+    doc.list <- strsplit(desc, "[[:space:]]+")
+    #stem.list <- lapply (doc.list, wordStem )
+    
+    
+    
+    
+    # compute the table of terms:
+    term.table <- table(unlist(doc.list))
+    term.table <- sort(term.table, decreasing = TRUE)
+    
+    # remove terms that are stop words or occur fewer than 5 times:
+    stop_words <- stopwords("SMART")
+    del <- names(term.table) %in% stop_words | term.table < 5 
+    
+    term.table <- term.table[!del]
+    
+    
+    
+    
+    
+    
+    
+    vocab <-  (names(term.table))
+    
+    
+    
+    
+    
+    
+    get.terms <- function(x,vocab) {
+      index <- match(x, vocab)
+      index <- index[!is.na(index)]
+      rbind(as.integer(index - 1), as.integer(rep(1, length(index))))
+    }
+    
+    ###### simple laplly  
+    
+    
+    documents <- lapply(doc.list, get.terms,vocab)
+    
+    
+    
+    
+    
+    
+    # Compute some statistics related to the data set:
+    D <- length(documents)  # number of documents  
+    W <- length(vocab)  # number of terms in the vocab  
+    doc.length <- sapply(documents, function(x) sum(x[2, ])) 
+    N <- sum(doc.length)  
+    term.frequency <- as.integer(term.table)
+    ########
+    
+    
+    
+    
+    
+    # MCMC and model tuning parameters:
+    K <- input$clust
+    G <- input$iter
+    
+    alpha <- input$ialpha
+    eta <- input$ieta
+    
+    inptop <-input$top
+    
+    # Fit the model:
+    
+    set.seed(357)
+    #t1 <- Sys.time()
+    
+    fit <- lda.collapsed.gibbs.sampler(documents = documents, K = K, vocab = vocab,
+                                       num.iterations = G, alpha = alpha,
+                                       eta = eta, initial = NULL, burnin = 0,
+                                       compute.log.likelihood = TRUE)
+    
+    
+    #t2 <- Sys.time()
+    #t2 - t1  
+    
+    
+    
+    topwords <- (top.topic.words(fit$topics, inptop, by.score=TRUE))
+    tw<-data.frame (topwords)
+    
+    
+    
+    
+    
+    formattable(tw,list())
+    
+    # 
+    # theta <- t(apply(fit$document_sums + alpha, 2, function(x) x/sum(x)))
+    # phi <- t(apply(t(fit$topics) + eta, 2, function(x) x/sum(x)))
+    # 
+    # lda_description <- list(phi = phi,
+    #                      theta = theta,
+    #                      doc.length = doc.length,
+    #                      vocab = vocab,
+    #                      term.frequency = term.frequency)
+    # 
+    # 
+    # json <- createJSON(phi = lda_description$phi, 
+    #                    theta = lda_description$theta, 
+    #                    doc.length = lda_description$doc.length, 
+    #                    vocab = lda_description$vocab, 
+    #                    term.frequency = lda_description$term.frequency)
+    # 
+    # 
+    # 
+    # #export the json file! 
+    # #write(json, "exported.json")
+    # 
+    # serVis(json, out.dir = 'vis2', open.browser = TRUE)
+    
+    
+    
+  })
+  
+  
+  
+  
 }
 
 
